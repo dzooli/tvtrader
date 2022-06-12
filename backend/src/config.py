08f80de4397bf7469ceeb1728ca73bb779b6e2db
Zoltan Fabian <zoltan.dzooli.fabian@gmail.com>
2022-06-12 14:03:14 +0200

    backend simplifiaction and CORS handling addition

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,19 +1,21 @@
 from ssl import ALERT_DESCRIPTION_DECODE_ERROR
 from sanic.config import Config
 
-class AppConfig(Config):
-    APPNAME="TradingView Trader"
-    PORT=8089
-    DEV=True
-    API_TITLE="TvTrader API",
-    OAS_IGNORE_OPTIONS=False,
-    CORS=True
-    SWAGGER_UI_CONFIGURATION = {
-            "apisSorter": "alpha",
-            # "jsonEditor": "true",
-            # "tryItOutEnabled": "true",
-            "operationsSorter": "alpha",
-            "docExpansion": "full",
-        },
 
-    
+class AppConfig(Config):
+    APPNAME = "TradingView Trader"
+    PORT = 8089
+    DEV = True
+    API_TITLE = "TvTrader API"
+    OAS_IGNORE_OPTIONS = False
+    CORS = True
+    CORS_ORIGINS = "http://localhost:8080"
+    CORS_METHODS = "GET,POST,OPTIONS"
+    CORS_AUTOMATIC_OPTIONS = True
+    SWAGGER_UI_CONFIGURATION = {
+        "apisSorter": "alpha",
+        "jsonEditor": "true",
+        "tryItOutEnabled": "true",
+        "operationsSorter": "alpha",
+        "docExpansion": "full",
+    }
--- backend/src/server.py
+++ backend/src/server.py
@@ -1,10 +1,26 @@
 from sanic import Sanic
+from sanic.response import json
+from sanic_ext import openapi
+from sanic.log import logger
 
 from config import AppConfig
-from views.Home import HomeView
 
-app = Sanic("TvTrader", config=AppConfig())
-app.add_route(HomeView.as_view(), "/")
+app = Sanic("TvTrader", config=AppConfig(),
+            configure_logging=True, )
+
+
+@app.get("/")
+@openapi.summary("Healthcheck endpoint")
+def check(request):
+    return json("HEALTHY " + app.config.APPNAME)
+
+
+@app.post("/")
+@openapi.summary("Alert POST endpoint. This is the endpoint for the Chrome extension.")
+async def post(request):
+    logger.info("POST received: " + str(request.body))
+    return json("OK")
+
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1",
