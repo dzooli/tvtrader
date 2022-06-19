40457f076d06a543cdd23fdfb849b45fc2f668f1
Zoltan Fabian <zoltan.dzooli.fabian@gmail.com>
2022-06-20 00:48:40 +0200

    added interval to the json schema

--- backend/src/server.py
+++ backend/src/server.py
@@ -24,11 +24,12 @@ def check(request):
 
 AlertSchema = {
     "type": "object",
-    "required": ["stratId", "stratName", "symbol", "direction", "timestamp"],
+    "required": ["stratId", "stratName", "symbol", "direction", "timestamp", "interval"],
     "properties": {
         "stratId":      {"type": "number", "minimum": 0},
         "stratName":    {"type": "string"},
         "symbol":       {"type": "string"},
+        "interval":     {"type": "number"},
         "direction":    {"type": "string", "maxLength": 4},
         "timestamp":    {"type": "string"}
     }
