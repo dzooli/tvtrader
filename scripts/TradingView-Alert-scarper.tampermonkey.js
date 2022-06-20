7c21bbcbaf6387cd86f282d219815d5660484beb
Zoltan Fabian <zoltan.dzooli.fabian@gmail.com>
2022-06-21 01:32:27 +0200

    fixed multiple POST requests based on the div's background-color style setting

--- scripts/TradingView-Alert-scarper.tampermonkey.js
+++ scripts/TradingView-Alert-scarper.tampermonkey.js
@@ -1,10 +1,9 @@
 // ==UserScript==
 // @name         TradingView - Alert scarper
 // @version      0.1
-// @description  Extracting TradingView alert text and sending it to http://localhost:8089 via JSON encoded POST. Alert panel must bwe open.
+// @description  Extracting TradingView alert text and sending it to http://localhost:8089 via JSON encoded POST. Alert panel must be open.
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js
 // @author       dzooli
-// @run-at       document-idle
 // @match        https://www.tradingview.com/chart/*
 // @connect      localhost
 // @grant        GM_xmlhttpRequest
@@ -12,45 +11,40 @@
 
 var $ = window.jQuery;
 
-(function() {
-    'use strict';
-    const checkAlert= setInterval(() => {
-        /** For development purposes, send test data **/
-        let testData = {
-            "stratId": 2,
-            "symbol": "FXCM:GBPUSD",
-            "stratName": "TEST",
-            "direction": "BUY",
-            "interval": 15,
-            "timestamp": "2019-08-27T09:56:00Z"
-        };
-        GM_xmlhttpRequest({
-            url: "http://localhost:8089/",
-            method: "POST",
-            timeout: 5,
-            contentType: "application/json",
-            data: JSON.stringify(testData),
+(function () {
+  "use strict";
+  const checkAlert = setInterval(() => {
+    const alertTimeout = 15 * 60;
+    const alertBody = $(
+      "div.widgetbar-widget-alerts_log > div.widgetbar-widgetbody > div > :nth-child(2) > :nth-child(2) > div"
+    ).first();
+    let alertText = alertBody.contents().text().trim();
+    if (alertText != "") {
+      let alert = JSON.parse(alertText);
+      let currTime = Date.now();
+      let alrtTime = new Date(alert.timestamp);
+      let alertTime = alrtTime.getTime();
+      let bgColor = alertBody.css("background-color").toString();
+      if (bgColor !== "rgb(128, 128, 128)" && bgColor !== "rgb(255, 0, 0)") {
+        alertBody.css({
+          background:
+            (currTime - alertTime) / 1000 < alertTimeout ? "red" : "gray",
         });
-        /** Dev test section end **/
-
-        const alertTimeout = 15 * 60;
-        const alertBody = $("div.widgetbar-widget-alerts_log > div.widgetbar-widgetbody > div > :nth-child(2) > :nth-child(2) > div").first();
-        let alertText = alertBody.contents().text().trim();
-        if (alertText != "") {
-            let alert = JSON.parse(alertText);
-            let currTime = Date.now();
-            let alrtTime = new Date(alert.timestamp);
-            let alertTime = alrtTime.getTime();
-            alertBody.css({
-                "background": (((currTime - alertTime) / 1000) < alertTimeout) ? "red" : "gray",
-            });
-            GM_xmlhttpRequest({
-                url: "http://localhost:8089/",
-                method: "POST",
-                timeout: 5,
-                contentType: "application/json",
-                data: JSON.stringify(alert),
-            });
-        }
-    }, 5000);
+        GM_xmlhttpRequest({
+          url: "http://localhost:8089/",
+          method: "POST",
+          timeout: 10000,
+          contentType: "application/json",
+          data: JSON.stringify(alert),
+          ontimeout: function (response) {
+            console.error("TradingView Alert Scarper: Alert POST timeout!");
+            alert(
+              "Manual check required! Sending alert has been timed out: " +
+                JSON.stringify(alert)
+            );
+          },
+        });
+      }
+    }
+  }, 5000);
 })();
