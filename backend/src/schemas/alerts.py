2a960e7724567bdcf69f1430bb8a59ca57c071f9
Zoltan Fabian <zoltan.dzooli.fabian@gmail.com>
2022-06-21 01:28:29 +0200

    JSON schema has been separated

---
+++ backend/src/schemas/alerts.py
@@ -0,0 +1,25 @@
+AlertSchema = {
+    "type": "object",
+    "required": ["stratId", "stratName", "symbol", "direction", "timestamp", "interval"],
+    "properties": {
+        "stratId":      {"type": "number", "minimum": 0},
+        "stratName":    {"type": "string"},
+        "symbol":       {"type": "string"},
+        "interval":     {"type": "number"},
+        "direction":    {"type": "string", "maxLength": 4},
+        "timestamp":    {"type": "string"}
+    }
+}
+
+ProcessedAlertSchema = {
+    "type": "object",
+    "required": ["stratId", "stratName", "symbol", "direction", "timestamp", "interval"],
+    "properties": {
+        "stratId":      {"type": "number", "minimum": 0},
+        "stratName":    {"type": "string"},
+        "symbol":       {"type": "string"},
+        "interval":     {"type": "number"},
+        "direction":    {"type": "string", "maxLength": 4},
+        "timestamp":    {"type": "number"}
+    }
+}
