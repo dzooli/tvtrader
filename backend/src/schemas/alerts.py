5e172b274b3ec90948d6607ad81371bbd462e1a4
Zoltan Fabian <zoltan.dzooli.fabian@gmail.com>
2022-06-23 16:29:41 +0200

    cleanup

--- backend/src/schemas/alerts.py
+++ backend/src/schemas/alerts.py
@@ -10,16 +10,3 @@ AlertSchema = {
         "timestamp":    {"type": "string"}
     }
 }
-
-ProcessedAlertSchema = {
-    "type": "object",
-    "required": ["stratId", "stratName", "symbol", "direction", "timestamp", "interval"],
-    "properties": {
-        "stratId":      {"type": "number", "minimum": 0},
-        "stratName":    {"type": "string"},
-        "symbol":       {"type": "string"},
-        "interval":     {"type": "number"},
-        "direction":    {"type": "string", "maxLength": 4},
-        "timestamp":    {"type": "number"}
-    }
-}
