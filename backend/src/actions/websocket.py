4d8cca8fef28d618d3401c513964c59ecd838c16
Zoltan Fabian <zoltan.dzooli.fabian@gmail.com>
2022-10-01 01:36:04 +0200

    separated websocket action

---
+++ backend/src/actions/websocket.py
@@ -0,0 +1,12 @@
+import json as js
+from typing import Dict, List
+from sanic.log import logger
+
+
+async def send_metric(jsondata: Dict, clients: List):
+    for iws in clients.copy():
+        try:
+            await iws.send(js.dumps(jsondata))
+        except Exception as ex:
+            logger.error("Failed to send the alert via WS! " + str(ex))
+            clients.remove(iws)
