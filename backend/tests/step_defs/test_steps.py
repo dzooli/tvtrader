29de9d2344e98348bf6faf872a4b4c35b55afdc2
Zoltan Fabian <zoltan.dzooli.fabian@gmail.com>
2022-11-05 15:17:54 +0100

    cleanup and generalized POST step

--- backend/tests/step_defs/test_steps.py
+++ backend/tests/step_defs/test_steps.py
@@ -10,16 +10,6 @@ from src.schemas.alerts import TradingViewAlert
 scenarios('../features')
 
 
-@when(parsers.parse('GET "{route}" route'), target_fixture="response")
-def step_get_route(base_url, route, capsys):
-    try:
-        resp = requests.get(f"{base_url}{route}")
-    except Exception:
-        assert False, "Connection failed!"
-    assert resp is not None, "Request failed!"
-    return resp
-
-
 @given(parsers.parse('alert with {id:n}, {name}, {symbol}, {interval:d}, {direction}, {price:f}, {timestamp}'), target_fixture="prepared_alert")
 def step_prepare_alert(id, name, symbol, interval, direction, price, timestamp):
     alert = TradingViewAlert(id, name, symbol, interval,
@@ -37,9 +27,9 @@ def step_remove_field(prepared_alert, fieldname, num_or_str="n", miss_or_inv="mi
     return alert
 
 
-@when('sending the prepared alert', target_fixture="response")
-def step_send_prepared(base_url, prepared_alert):
+@when(parsers.parse('sending the prepared alert to endpoint: {endpoint}'), target_fixture="response")
+def step_send_prepared(base_url, prepared_alert, endpoint):
     send_alert = prepared_alert
     if isinstance(prepared_alert, TradingViewAlert):
         send_alert = asdict(prepared_alert)
-    return requests.post(f"{base_url}/alert", json=send_alert)
+    return requests.post(f"{base_url}/{endpoint}", json=send_alert)
