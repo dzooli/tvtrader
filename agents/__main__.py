d26f487a0a58636bbc91ae07bfbe0d092b0c4f5c
Zoltan Fabian <zoltan.dzooli.fabian@gmail.com>
2023-10-25 06:01:13 +0200

    move towards click + dynamic loading impr.

--- agents/__main__.py
+++ agents/__main__.py
@@ -1,60 +1,110 @@
+import importlib
 import logging
-import wsaccel
-import ws4py
 import sys
-from pprint import pprint
-from importlib import import_module
+import typing
+
 import click
+import ws4py
 from click_default_group import DefaultGroup
 
-from tvt_agents.distributor.source import WebSocketSource
-from tvt_agents.distributor import Distributor
+
+def setup_logging(level: int = logging.DEBUG):
+    res_logger = ws4py.configure_logger(level=level)
+    fmt = logging.Formatter(
+        "%(asctime)s.%(msecs)03d - %(levelname)s - %(filename)s:%(lineno)s - %(message)s"
+    )
+    for h in res_logger.handlers:
+        h.setFormatter(fmt)
+    return res_logger
 
 
-@click.group(cls=DefaultGroup, default='start', default_if_no_args=True)
+# # Logging configuration example without ws4py
+# logging.basicConfig(
+#     format="%(asctime)s.%(msecs)03d - %(levelname)s - %(filename)s:%(lineno)s - %(message)s",
+#     datefmt="%Y-%m-%d %H:%M:%S",
+#     level=logging.DEBUG,
+# )
+# logger = logging.getLogger()
+
+logger = setup_logging()
+dist_targets: typing.Set[object] = set()
+
+
+def collect_dist_targets(src: str = "targets"):
+    logger.info("Collecting custom targets...")
+    targets_module = importlib.import_module(src, src)
+    for name in targets_module.__all__:
+        if (
+            callable(eval(f"targets_module.{name}"))
+            and name.startswith("create_")
+            and name.endswith("_target")
+        ):
+            logger.info(f"Found target: {name}. Registration...")
+            try:
+                obj = eval(f"targets_module.{name}")()
+            except Exception:
+                logger.critical(
+                    f"Cannot instantiate the distributor target with: {name}"
+                )
+            logger.info(f"{name} has been registered as {obj}")
+            dist_targets.add(obj)
+
+
+@click.group
+def show():
+    pass
+
+
+@show.command
+@click.option(
+    "--verbose", default=False, help="Display target's documentation if exists."
+)
+def targets(verbose):
+    collect_dist_targets()
+    print("\nInitialized targets: ")
+    for obj in dist_targets:
+        print("-", type(obj), "from module:", obj.__module__)
+        if verbose:
+            print("  Documentation:")
+            print("   ", obj.__doc__)
+
+
+@click.group(cls=DefaultGroup, default="start", default_if_no_args=True)
 def cli():
     pass
 
 
 @cli.command
 def start(src: str = "targets"):
-    print('Importing custom targets...')
-    targets = import_module(src, src)
-    for name in targets.__all__:
-        print(name, type(name))
-        if callable(eval(f"targets.{name}")) and name.startswith('create_') and name.endswith('_target'):
-            print(f"{name} is callable")
-            obj = eval(f"targets.{name}")()
-            print(obj)
+    collect_dist_targets(src)
+    logger.info("Starting the distributor...")
 
 
+# Test for dynamically created targets
 if __name__ == "__main__":
+    cli.add_command(show)
     cli()
-    exit()
+    sys.exit()
 
 
-def setup_logging(level: int = logging.DEBUG):
-    res_logger = ws4py.configure_logger(level=level)
-    fmt = logging.Formatter("| [%(asctime)s] | %(levelname)8s | %(message)s")
-    for h in res_logger.handlers:
-        h.setFormatter(fmt)
-    return res_logger
+# Basic usage without dynamic import
+# import wsaccel
+# from tvt_agents.distributor import Distributor
+# from tvt_agents.distributor.source.websocket import WebSocketSource
+# logger = setup_logging()
+# def run_loop():
+#     wsaccel.patch_ws4py()
+#     dist = Distributor()
+#     dist.logger = logger
+#     ws_source = WebSocketSource(
+#         "wss://socketsbay.com/wss/v2/1/demo/",
+#         protocols=["http-only", "chat"],
+#     )
+#     ws_source.logger = logger
+#     dist.add_source(ws_source)
+#     dist.connect()
 
-
-def run_loop():
-    logger = setup_logging()
-    wsaccel.patch_ws4py()
-    dist = Distributor()
-    dist.logger = logger
-    ws_source = WebSocketSource(
-        "wss://socketsbay.com/wss/v2/1/demo/",
-        protocols=["http-only", "chat"],
-    )
-    ws_source.logger = logger
-    dist.add_source(ws_source)
-    dist.connect()
-
-    try:
-        dist.run()
-    except KeyboardInterrupt:
-        dist.shutdown()
+#     try:
+#         dist.run()
+#     except KeyboardInterrupt:
+#         dist.shutdown()
