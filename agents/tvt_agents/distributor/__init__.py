611b0b22a4cd36c4892ac491116097b30db8209f
Zoltan Fabian <zoltan.dzooli.fabian@gmail.com>
2023-10-25 06:11:26 +0200

    solved task finishing

--- agents/tvt_agents/distributor/__init__.py
+++ agents/tvt_agents/distributor/__init__.py
@@ -6,7 +6,6 @@
 Alert distributor with multiple sources and targets
 
 """
-import logging
 import queue
 from queue import Queue
 from time import sleep
@@ -16,6 +15,7 @@ from attrs import define, field, validators
 
 from .source import AbstractDistributionSource
 from .target import AbstractDistributionTarget
+from .exceptions import QueueEmpty
 from .logutil import LoggingMixin
 
 
@@ -30,9 +30,10 @@ class Distributor(LoggingMixin):
     Targets must implement AbstractDistributionTarget and
     sources must implement AbstractDistributionSource.
     """
+
     _shutdown_progress: bool = field(default=False)
     _queue: Queue = field(factory=Queue)
-    _src_threadlist: list = field(factory=list)
+    _src_threadlist: list[Thread] = field(factory=list)
     _sources: List[AbstractDistributionSource] = field(factory=list)
     _targets: List[AbstractDistributionTarget] = field(factory=list)
     _send_delay: float = field(default=0.2, validator=validators.gt(0.0))
@@ -48,15 +49,15 @@ class Distributor(LoggingMixin):
     def add_source(self, src: AbstractDistributionSource):
         self._sources.append(src)
         self._sources[-1].set_on_message(self._thread_enqueue)
-        self.log(logging.INFO, "source added")
+        self.logger.info("source added")
 
     def connect_sources(self):
-        self.log(logging.DEBUG, "connecting sources...")
+        self.logger.debug("connecting sources...")
         for src in self._sources:
-            src.connect()
+            src.open()
 
     def connect_targets(self):
-        self.log(logging.DEBUG, "connecting targets...")
+        self.logger.debug("connecting targets...")
         for tgt in self._targets:
             tgt.open()
 
@@ -66,52 +67,57 @@ class Distributor(LoggingMixin):
 
     def add_target(self, tgt: AbstractDistributionTarget):
         self._targets.append(tgt)
-        self.log(logging.INFO, "target added")
+        self.logger.info("target added")
 
     def _enqueue(self, message):
         if self._shutdown_progress:
-            self.log(logging.WARNING, "No new messages accepted, shutdown in progress")
+            self.logger.warning("No new messages accepted, shutdown in progress.")
             return
         try:
             self._queue.put_nowait(str(message))
         except queue.Full:
-            self.log(logging.ERROR, "failed to enqueue the message! Queue is full")
+            self.logger.error("failed to enqueue the message! Queue is full.")
             return
-        self.log(logging.INFO, "message enqueued...")
+        self.logger.info("message enqueued...")
 
     def _thread_enqueue(self, message):
         if self._shutdown_progress:
-            self.log(logging.WARNING, "No new messages threaded, shutdown in progress")
+            self.logger.warning("No new messages threaded, shutdown in progress.")
             return
-        self._src_threadlist.append(Thread(target=self._enqueue, kwargs={"message": message}))
-        self.log(logging.DEBUG, "Starting equeue thread...")
+        self._src_threadlist.append(
+            Thread(target=self._enqueue, kwargs={"message": message})
+        )
+        self.logger.debug("Starting equeue thread...")
         self._src_threadlist[-1].start()
-        # TODO: Prevent to fill the memory with finished threads
 
     def run(self):
         while True:
-            while not self._queue.empty():
-                last_msg = self._queue.get()
-                self._send_to_all(last_msg)
-                self._queue.task_done()
+            try:
+                last_msg = self._queue.get(block=False)
+            except queue.Empty:
+                sleep(self._send_delay)
+                continue
+            self._distribute_to_all(last_msg)
+            self._queue.task_done()
             sleep(self._send_delay)
 
-    def _send_to_all(self, message):
-        self.log(logging.DEBUG, f"sending message '{message}' to all targets...")
+    def _distribute_to_all(self, message):
+        self.logger.debug(f"sending message '{message}' to all targets...")
         for tgt in self._targets:
-            tgt.send(message)
-            self.log(logging.INFO, "message sent")
+            tgt.distribute(message)
+            self.logger.info("message sent")
 
     def flush(self):
-        self.log(logging.INFO, "Flushing the queue...")
+        self.logger.info("Flushing the queue...")
         while not self._queue.empty():
-            last_msg = self._queue.get()
-            self._send_to_all(last_msg)
+            last_msg = self._queue.get(block=False)
+            self._distribute_to_all(last_msg)
             self._queue.task_done()
+            sleep(self._send_delay)
 
     def shutdown(self):
         self._shutdown_progress = True
-        self.log(logging.INFO, "closing sources...")
+        self.logger.info("closing sources...")
         for src in self._sources.copy():
             src.close(
                 code=AbstractDistributionSource.DISCONNECT_SHUTDOWN,
@@ -120,11 +126,11 @@ class Distributor(LoggingMixin):
 
         self.flush()
 
-        self.log(logging.INFO, "closing targets...")
+        self.logger.info("closing targets...")
         for tgt in self._targets.copy():
             tgt.close()
 
-        self.log(logging.INFO, "Waiting for queue threads to finish...")
+        self.logger.info("Waiting for queue threads to finish...")
         # shutting down the source queue threads
         for c_thread in self._src_threadlist:
             c_thread.join()
