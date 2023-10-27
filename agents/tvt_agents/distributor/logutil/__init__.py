7cfb3640c3b66a1c6011dba6e8649137c98ebdc7
Zoltan Fabian <zoltan.dzooli.fabian@gmail.com>
2023-10-27 04:49:17 +0200

    fixed exception upon missing initialization

--- agents/tvt_agents/distributor/logutil/__init__.py
+++ agents/tvt_agents/distributor/logutil/__init__.py
@@ -9,26 +9,29 @@ from attrs import define, field, validators
 class LoggingMixin:
     """Use this logger trait where needed."""
 
-    _logger: logging.Logger = field(
+    _logger: logging.Logger | None = field(
         init=True,
         validator=validators.optional(validators.instance_of(logging.Logger)),
     )
 
+    def __init__(self):
+        self._logger = None
+
     @property
     def logger(self):
         """The underlying logger."""
-        return self._logger
+        try:
+            return self._logger
+        except AttributeError:
+            return None
 
     @logger.setter
     def logger(self, logger: logging.Logger):
         """Set the logger."""
-        if issubclass(logger.__class__, logging.Logger):
-            self._logger = logger
+        self._logger = logger
 
     def log(self, level: int, item) -> bool:
         """Use the logger."""
-        if not self._logger:
-            return False
         try:
             logger_fn = getattr(self._logger, str(logging.getLevelName(level)).lower())
         except AttributeError:
