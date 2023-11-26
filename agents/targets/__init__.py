e9a76594a05c385db0cdff85ee885e845cbeb2d5
Zoltan Fabian <zoltan.dzooli.fabian@gmail.com>
2023-11-26 22:43:33 +0100

    separation started

--- agents/targets/__init__.py
+++
@@ -1,5 +0,0 @@
-from .console import create_console_target
-
-__all__ = [
-    "create_console_target",
-]
--- backend/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source venv/Scripts/activate
-python -m src.server
-# alternative: using Sanic CLI
-sanic -d -p 8089 --fast src.server:app
